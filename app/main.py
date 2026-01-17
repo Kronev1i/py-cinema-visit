46be6c315920ebe0bba1637173f135d90f9a1551
unknown <kgoncharenko83@gmail.com>
2026-01-17 21:42:18 +0100

    Added missing spaces

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ def cinema_visit(customers: list,
         customer = Customer(name=data["name"], food=data["food"])
         customer_object.append(customer)
         CinemaBar.sell_product(product=customer.food, customer=customer)
-    hall=CinemaHall(hall_number)
-    cleaner_obj=Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
+    cleaner_obj = Cleaner(cleaner)
 
     hall.movie_session(
         movie_name=movie,
